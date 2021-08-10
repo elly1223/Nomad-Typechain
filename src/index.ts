a8add9225ea9cd44c4199611b99f8211bd1ab6aa
yunkyung <poew91@gmail.com>
2021-08-10 18:44:59 +0900

    class

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,28 @@
-interface Human {
-  // interface는 typescript에서만 작동한다!
-  name: string;
-  age: number;
-  gender: string;
+class Human {
+  public name: string;
+  private age: number;
+  public gender: string;
+  constructor(name: string, age: number, gender: string) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
 }
 
-const person = {
-  name: 'nicolas',
-  age: 22,
-  gender: 'male',
-};
+const lynn = new Human('Lynn', 19, 'female');
+
+// interface Human {
+//   // interface는 typescript에서만 작동한다!
+//   name: string;
+//   age: number;
+//   gender: string;
+// }
+
+// const person = {
+//   name: 'nicolas',
+//   age: 22,
+//   gender: 'male',
+// };
 
 const sayHi = (person: Human): string => {
   // gender parameter는 선택적이게 된다!
@@ -17,6 +30,6 @@ const sayHi = (person: Human): string => {
 };
 
 // const sayHi: (name: string, age: number, gender: string) => void;
-console.log(sayHi(person));
+console.log(sayHi(lynn));
 
 export {};
